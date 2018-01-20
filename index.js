f4243606ecf86af65a7ef113e24c058435af3d9a
Skylar Dunn-Lubin <skylar.dunnlubin@gmail.com>
2018-01-20 23:57:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,15 +4,13 @@ function happyHolidays(){
 }
 
 function happyHolidaysTo(name) {
-  return `Happy holidays, ${name}!`}
+  return `Happy holidays, ${name}!`
+}
 
 function happyHolidayTo(holiday, name){
-  return 'Happy `${holiday}, ${name}!'
-}
-
-happyHolidayTo('Independence Day', 'you')
-
+  return `Happy `${holiday}, ${name}!`
+  }
+  
 function holidayCountdown(days, holiday){
-  return 'It\'s ${days} days until ${holiday}!`'
+  return `It\'s ${days} days until ${holiday}!``
 }
-holidayCountdown('20', 'Mother\'s Day')
